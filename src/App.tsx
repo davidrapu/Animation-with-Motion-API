624c31ba52a16fe04152dec278f8d4f1a91b8114
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-30 21:38:36 +0000

    Refactor App component layout and add animation components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import Fade from "./Animations/Fade";
+import Hovering from "./Animations/Hovering";
+import PositionChange from "./Animations/PositionChange";
+import StaggeringChildren from "./Animations/StaggeringChildren";
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="bg-[#9F8383] w-200 h-fit p-4 flex flex-col gap-y-8 justify-center items-center rounded-4xl">
+      <Fade />
+      <PositionChange />
+      <Hovering />
+      <StaggeringChildren />
     </div>
-  )
+  );
 }
